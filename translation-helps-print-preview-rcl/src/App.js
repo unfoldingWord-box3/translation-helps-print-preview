fd15e9d766b58f7fed3e278ef6e455821b270c5e
klappy <christopher@klapp.name>
2022-06-14 15:06:08 -0400

    created path for app

---
+++ translation-helps-print-preview-rcl/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Translation Helps Print Preview App</h1>
+        <p>This is a placeholder. Go update it to make it do something.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
