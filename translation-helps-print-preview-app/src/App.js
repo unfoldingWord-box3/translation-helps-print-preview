672ba93535bafde004c19607387434b5bb12a3ef
Abel Perez <abelper54@gmail.com>
2022-06-16 14:03:15 -0400

    add permalinks

--- translation-helps-print-preview-app/src/App.js
+++ translation-helps-print-preview-app/src/App.js
@@ -1,28 +1,15 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 import Layout from './components/Layout';
+import { PermalinksHandler } from './components/PermalinksHandler';
 
 function App() {
-  const [url, setUrl] = useState('https://git.door43.org/test_org2/ml_tn/raw/branch/master/tn_3JN.tsv');
-
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const _url = urlParams.get('url');
-    
-    console.log(_url);
-    setUrl(_url);
-
-    return () => {
-      setUrl();
-    };
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Translation Helps Print Preview App</h1>
-      <Layout url={url} />
-    </div>
+    <PermalinksHandler>
+      <div className="App">
+        <h1>Translation Helps Print Preview App</h1>
+        <Layout/>
+      </div>
+    </PermalinksHandler>
   );
 }
 
---
+++ translation-helps-print-preview-app/src/hooks/useFile.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useMemo, useState } from 'react'
+
+export default function useFile(permalink) {
+  const { owner, repository, branch, filePath } = permalink || {};
+  const [file, setFile] = useState();
+
+  const basePath = "https://git.door43.org";
+
+  const url = useMemo(() => permalink && [basePath, owner, repository, "raw", "branch", branch, filePath].filter(Boolean).join("/"), [owner, repository, branch, filePath, permalink]);
+  
+  useEffect(() => {
+    if(url)
+    fetch(url)
+    .then(response => response.text())
+    .then(_file => setFile(_file));
+
+    return (() => {
+      setFile();
+    });
+  }, [url]);
+
+
+  return file;
+}
